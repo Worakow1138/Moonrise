c003f0dd9635c0658931f401b4802b1f4a90f603
Christopher Diamond-Jones <christopher.jones1138@gmail.com>
2023-04-20 16:06:52 -0400

    Update Moon_Browser.py
    Move import logic to use_current_browser() method.
    
    Only create session_info_file if module is located (not from an executable)
    
    Remove session_info.py when calling cleanup_browser()

--- src/Moonrise/Moon_Browser.py
+++ src/Moonrise/Moon_Browser.py
@@ -3,10 +3,6 @@ import subprocess
 from selenium import webdriver
 from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
 from selenium.webdriver.common.service import Service
-try:
-    from Moonrise import session_info
-except ImportError:
-    pass
 
 
 
@@ -59,14 +55,22 @@ class MoonBrowser:
         self.moon_driver = browser_options[browser_type]['webdriver_create'](options=options)
 
         # write executor_url and session_id to a file named session_info.py for future use
-        session_info_file = open(str(os.getcwd())+'\\session_info.py', 'w')
-        session_info_file.write(f'executor_url="{self.moon_driver.command_executor._url}"\nsession_id="{self.moon_driver.session_id}"')
-        session_info_file.close()
+        try:
+            session_info_file = open(os.path.dirname(os.path.realpath(__file__))+'\\session_info.py', 'w')
+            session_info_file.write(f'executor_url="{self.moon_driver.command_executor._url}"\nsession_id="{self.moon_driver.session_id}"')
+            session_info_file.close()
+        except FileNotFoundError:
+            pass
 
     def use_current_browser(self):
         """Allows for test executions to begin on the last opened browser
            Also creates the moon_driver variable for access to selenium webdriver methods
         """
+
+        try:
+            from Moonrise import session_info
+        except ImportError:
+            raise ImportError("No open browser sessions.")
 
         # To prevent a new browser session from being created, we need to temporarily overwrite the RemoteWebDriver.execute method
         # Save the original function, so we can revert our patch
@@ -95,6 +99,7 @@ class MoonBrowser:
         try:
             self.moon_driver.quit()
             self.moon_driver = None
+            os.remove(os.path.dirname(os.path.realpath(__file__))+'\\session_info.py')
         except:
             pass
         subprocess.call('taskkill /f /im geckodriver.exe', stdout=open(os.devnull, "wb"), stderr=open(os.devnull, "wb"))
